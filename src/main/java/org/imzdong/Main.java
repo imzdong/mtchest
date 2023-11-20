755df5646b689306ca9f7df1c7b136e60825df37
imzdong <zhoudong_dream@163.com>
2023-11-20 10:34:16 +0800

    init

---
+++ src/main/java/org/imzdong/Main.java
@@ -0,0 +1,12 @@
+package org.imzdong;
+
+/**
+ * @author DongZhou$
+ * @date 2023/11/20$ 10:01$
+ * @description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
